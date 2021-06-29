143a3287c99e8bfaa1f21262cfe54c2279c9b74a
dinakar29 <26552821+dinakar29@users.noreply.github.com>
2021-06-28 20:10:37 -0500

    Fix issues

--- r-license-report/entrypoint.sh
+++ r-license-report/entrypoint.sh
@@ -1,15 +1,35 @@
 #!/usr/bin/env bash
 
-FAIL_ARG=""
+ARGS=""
+
 if [[ "${INPUT_FAIL}" == "true" || "${INPUT_FAIL}" == "1" ]]
 then {
-    FAIL_ARG="-f"
+    ARGS="${ARGS} -f"
 }
 fi
 
-Rscript /main.R \
-    -p "${INPUT_PATH}" \
-    -r "${INPUT_REGEX}" \
-    ${FAIL_ARG} \
-    -s "${INPUT_MRAN_SNAPSHOT_DATE}" \
-    -b "${INPUT_BIOC_RELEASE}"
+if [[ -z "${INPUT_PATH}" ]]
+then {
+    ARGS="${ARGS} -p ${INPUT_PATH}"
+}
+fi
+
+if [[ -z "${INPUT_REGEX}" ]]
+then {
+    ARGS="${ARGS} -r ${INPUT_REGEX}"
+}
+fi
+
+if [[ -z "${INPUT_MRAN_SNAPSHOT_DATE}" ]]
+then {
+    ARGS="${ARGS} -s ${INPUT_MRAN_SNAPSHOT_DATE}"
+}
+fi
+
+if [[ -z "${INPUT_BIOC_RELEASE}" ]]
+then {
+    ARGS="${ARGS} -b ${INPUT_BIOC_RELEASE}"
+}
+fi
+
+Rscript /main.R "${ARGS}"
